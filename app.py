b1ae0d4e611f4535fa57e051952da2a95734ec5e
RSV-exp <rapellysaivinil@gmail.com>
2026-03-22 09:36:26 +0530

    Made route production ready

--- app.py
+++ app.py
@@ -471,9 +471,8 @@ def report():
 
 
 
-if __name__ == '__main__':
-   app.run(
-    host='0.0.0.0',
-    port=5000,
-    debug=True
-)
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    debug_mode = os.environ.get("FLASK_DEBUG", "False").lower() == "true"
+    app.run(host="0.0.0.0", port=port, debug=debug_mode)
